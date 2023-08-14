44ec50e2e8324391ce91fc3179bd1c11f62b3f35
nongfugengxia <nongfugengxia@163.com>
2023-08-14 15:58:44 +0800

    将翻译结果部署到软件中

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RaodRunner 的中文翻译文档
 该项目属于自定义工业 [仿真软件](https://github.com/OpenHUTB/matlab) [帮助文档](https://github.com/OpenHUTB/matlab/tree/master/help) 的一部分。
 
-1. 将网页对应的文件名保存为本地的`.mlx`文件，并用`export2html.m`脚本导出为网页文件，并部署到当前使用软件的对应目录；
+1. 将网页对应的文件名保存为本地的`.mlx`文件，并用`deploy.m`脚本导出为网页文件，并部署到当前使用软件的对应目录；
 2. 使用命令`doc roadrunner`进行测试；
 
 
--- deploy.m
+++ deploy.m
@@ -4,6 +4,8 @@ cur_dir = pwd;
 % 得到当前目录下的所有.mlx文件
 all_dir = genpath(cur_dir);
 all_dirs = split(all_dir, ';');
+
+%% 将.mlx文件导出为网页文件
 % 获取所有的目录
 for i = 1 : numel(all_dirs)-1
     cur_mlx_dir = all_dirs{i};
@@ -15,16 +17,18 @@ for i = 1 : numel(all_dirs)-1
         % 原始.mlx文件的路径
         cur_mlx_path = fullfile(cur_mlx_struct.folder, cur_mlx_name);
         % 生成的网页文件的路径
+        cur_mlx_folder = cur_mlx_struct.folder;
         dst_html_path = fullfile(cur_mlx_struct.folder, ...
             [cur_mlx_name(1:length(cur_mlx_name)-3), 'html']);
         % 导出网页格式到和.mlx相同的文件夹
-        export(cur_mlx_path, [cur_mlx_path(1:length(cur_mlx_path)-3) '.html']);
+        export(cur_mlx_path, dst_html_path);
+
+        % 将翻译结果部署到软件中
+        start_pos = strfind(cur_mlx_folder, 'roadrunner\');
+        matlab_html_dir = fullfile(matlabroot, "help", cur_mlx_folder(19:end));
+        copyfile(dst_html_path, matlab_html_dir);
     end
 end
-% all_dirs = all_dirs{1 : numel(all_dirs)-1};
 
 
 
-
-%% 将该工程所有的翻译结果单独部署到软件中
-% 使用https://github.com/OpenHUTB/matlab进行部署则不需要单独部署
